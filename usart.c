78b24781ffe551c61a6769e514504504c107aece
Екатерина Коннова <ekonnova@ics.local>
2018-02-13 11:25:21 +0300

    lcd print

--- usart.c
+++ usart.c
@@ -12,8 +12,6 @@
 #include "ADC_init.h"
 #include "LCD_init.h"
 #include "LCD2_init.h"
-#include "stdio.h"
-#include "DMA_INIT.h"
  
 #define RX_BUF_SIZE 80
 
@@ -23,10 +21,7 @@
  char RXi;
  char RXc;
  char RX_BUF[RX_BUF_SIZE] = {'\0'};
- char buffer[5] = {'\0'};
- char AddBuf[32];
- char AddBuf1[32] = {'\0'};
- 
+ char buffer[80] = {'\0'};
  
 
 	uint32_t  ADC_out1;
@@ -144,173 +139,175 @@ void SetSysClockTo72(void)
 int main(void)
 {
     // Set System clock
-		SystemInit();
-		SetSysClockTo72();
-		DelayInit();
-		usart_init();
-		ADC_init();
-		i2c_init();
-		LCD2_init();
-	 // ADC_DMA_init();
-	 //lcd_i2c_init();
-	
-	
-		LCD_W(" SEREGA MOLODEC {} ,. >< <> &%$#sdfsdgsgs1241515adhadhadhadhdf@");
-		LCD_DEC(C1);
-		_delay_ms(2000);
-		LCD_CLEAR();
-		_delay_ms(2000);
-	  LCD_ADR (0x54);
-		// line 1: 0x80
-		// line 2: 0x40
-		// line 3: 0x94
-		// line 4: 0x54
-		uint8_t Transmit_Buffer[9];
+			SystemInit();
+			SetSysClockTo72();
+			DelayInit();
+			usart_init();
+			ADC_init();
+			//i2c_init();
+			//LCD2_init();
+			// lcd_i2c_init();
+			LCD_W(" SEREGA MOLODEC {} ,. >< <> &%$#sdfsdgsgs1241515adhadhadhadhdf@");
+			LCD_DEC(C1);
+			_delay_ms(2000);
+			LCD_CLEAR();
+			_delay_ms(2000);
+			LCD_ADR (0x54);
+			// line 1: 0x80
+			// line 2: 0x40
+			// line 3: 0x94
+			// line 4: 0x54
 	 
 	
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2,ENABLE);
-    /* Initialize LED which connected to PC13 */
-    GPIO_InitTypeDef  GPIO_InitStructure;
-    // Enable PORTC Clock
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    /* Configure the GPIO_LED pin */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOC, &GPIO_InitStructure);
- 
-    GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
-		
-		char BUF_2;
-    // Initialize USART
-    usart_init();
-    USARTSend(" Hello.\r\nUSART1 is ready.\r\n");
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1,ENABLE);
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC2,ENABLE);
+			/* Initialize LED which connected to PC13 */
+			GPIO_InitTypeDef  GPIO_InitStructure;
+			// Enable PORTC Clock
+			RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+			/* Configure the GPIO_LED pin */
+			GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+			GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+			GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+			GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+			GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+
+			// Initialize USART
+			usart_init();
+			USARTSend(" Hello.\r\nUSART1 is ready.\r\n");
  
     while (1)
-    {		
+    {		//RX_BUF_TXT=RX_BUF;
         if (RX_FLAG_END_LINE == 1) {
+            // Reset END_LINE Flag
             RX_FLAG_END_LINE = 0;
  
             USARTSend("\r\nI has received a line:\r\n");
             USARTSend(RX_BUF);
             USARTSend("\r\n");
  
-					if (strncmp(RX_BUF, "ON\r", 3) == 0) {
+            if (strncmp(RX_BUF, "ON\r", 2) == 0) {
                 USARTSend("\r\nTHIS IS A COMMAND \"ON\"!!!\r\n");
                 GPIO_ResetBits(GPIOC, GPIO_Pin_13);			
             }
             if (strncmp(RX_BUF, "OFF\r", 3) == 0) {
                 USARTSend("\r\nTHIS IS A COMMAND \"OFF\"!!!\r\n");
                 GPIO_SetBits(GPIOC, GPIO_Pin_13);
-						}
-						if (strncmp(RX_BUF, "LCD_CLEAR\r", 8) == 0) {
-								USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
-						LCD_CLEAR();
-						BUF_2=254;
-            sprintf(buffer, "\r\n%d ", BUF_2);
-            USARTSend(buffer);
+			}
+			if (strncmp(RX_BUF, "LCD_CLEAR\r", 8) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
+				LCD_CLEAR();
             }
-						if (strncmp(RX_BUF, "PRINT", 4) == 0) {
-						USARTSend("\r\nTHIS IS A COMMAND \"PRINT\"!!!\r\n");
-						LCD_CLEAR();
-						for (RXi=6; RXi<RX_BUF_SIZE; RXi++)
-							AddBuf1[RXi] = RX_BUF[RXi];
-            sprintf(AddBuf, "\r\n%s ", AddBuf1);
-            USARTSend(AddBuf);
-						LCD_W(AddBuf);
+			if (strncmp(RX_BUF, "BATA_V_HATE\r", 8) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
+				LCD_CLEAR();
+				ADC_out1=(int)ADC_out1/10;
+				ADC_out2=(int)ADC_out2/10;
+				ADC_out1_V=ADC_out1/1000;
+				ADC_out2_V=ADC_out2/1000;
+				ADC_out1_mV=1000+555;
+				ADC_out2_mV=1000+555;
+				LCD_W("VOLTAGE     CURRENT ");
+				LCD_W("                     ");
+				LCD_DEC(ADC_out1_mV);
+				LCD_W("       ");
+				LCD_DEC(RXc);
+				for (RXi=0; RXi<RX_BUF_SIZE; RXi++)
+					{LCD_DEC(RX_BUF[RXi]);}
             }
-						if (strncmp(RX_BUF, "BATA_V_HATE\r", 8) == 0) {
-								sprintf((char*)Transmit_Buffer, "ADC=%d", ADC_out1);
-							 USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
-							 //BUF_2=0x33;
-							// USARTSend(&BUF_2);
-						 	 BUF_2=0x03;
-							 USARTSend(&BUF_2);
-							 BUF_2=0x08;
-							 USARTSend(&BUF_2);
-							 LCD_CLEAR();
-													ADC_out1=(int)ADC_out1/10;
-													ADC_out2=(int)ADC_out2/10;
-													ADC_out1_V=ADC_out1/1000;
-													ADC_out2_V=ADC_out2/1000;
-													ADC_out1_mV=1000+555;
-													ADC_out2_mV=1000+555;
-							LCD_W("VOLTAGE     CURRENT ");
-							LCD_W("                     ");
-							LCD_DEC(ADC_out1_mV);
-							LCD_W("       ");
-						
+			if (strncmp(RX_BUF, "PRINT CHAR", 10) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"PRINT\"!!!\r\n");
+				LCD_CLEAR();
+				for(i=0;i<RX_BUF_SIZE;i++)
+				{
+					buffer[i]=RX_BUF[i+5];
+				}
+				LCD_W("You wrote: \r\n");
+				LCD_W(buffer);
+				
             }
-						if (strncmp(RX_BUF, "lcd_i2c_clear\r", 10) == 0) {
-            USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
-			lcd_i2c_clear();
+			if (strncmp(RX_BUF, "PRINT NUM", 9) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"PRINT\"!!!\r\n");
+				LCD_CLEAR();
+				for(i=0;i<RX_BUF_SIZE;i++)
+				{
+					buffer[i]=RX_BUF[i+9];
+				}
+				long int num;
+				sscanf(buffer, "%ld", &num);
+				LCD_W("You wrote: \r\n");
+				LCD_DEC(num);
+				
             }
-									if (strncmp(RX_BUF, "ADC\r", 8) == 0) {
-            USARTSend("\r\nTHIS IS A COMMAND \"ADC\"!!!\r\n");
-			lcd_i2c_clear();
-			lcd_i2c_w(RX_BUF);
+			if (strncmp(RX_BUF, "lcd_i2c_clear\r", 10) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
+				lcd_i2c_clear();
+            }
+			if (strncmp(RX_BUF, "ADC\r", 8) == 0) {
+				USARTSend("\r\nTHIS IS A COMMAND \"ADC\"!!!\r\n");
+				lcd_i2c_clear();
+				lcd_i2c_w(RX_BUF);
 									}
 			if (strncmp(RX_BUF, "TXT\r", 8) == 0) {
-            USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
-			lcd_i2c_clear();
-			lcd_i2c_w(RX_BUF);
+				USARTSend("\r\nTHIS IS A COMMAND \"LCD clear\"!!!\r\n");
+				lcd_i2c_clear();
+				lcd_i2c_w(RX_BUF);
 			}
 			
-						 if (strncmp(RX_BUF, "lcd_i2c_adc\r", 10) == 0) {
+			if (strncmp(RX_BUF, "lcd_i2c_adc\r", 10) == 0) {
                 USARTSend("\r\nTHIS IS A COMMAND \"LCD\"!!!\r\n");
-                ADC_out1=0x33;
-							 USARTSend((char*)ADC_out1);
-						ADC_out1=(int)ADC_out1/10;
-						ADC_out2=(int)ADC_out2/10;
-						ADC_out1_V=ADC_out1/1000;
-						ADC_out2_V=ADC_out2/1000;
-						ADC_out1_mV=ADC_out1%1000+555;
-						ADC_out2_mV=ADC_out2%1000+555;
-						lcd_i2c_command(0x02);
-						lcd_i2c_w("VOLTAGE  CURRENT");
-						lcd_i2c_adr(0x40);
-						lcd_i2c_dec(ADC_out1_V);
-						lcd_i2c_w(",");
+                
+				ADC_out1=(int)ADC_out1/10;
+				ADC_out2=(int)ADC_out2/10;
+				ADC_out1_V=ADC_out1/1000;
+				ADC_out2_V=ADC_out2/1000;
+				ADC_out1_mV=ADC_out1%1000+555;
+				ADC_out2_mV=ADC_out2%1000+555;
+				lcd_i2c_command(0x02);
+				lcd_i2c_w("VOLTAGE  CURRENT");
+				lcd_i2c_adr(0x40);
+				lcd_i2c_dec(ADC_out1_V);
+				lcd_i2c_w(",");
 
-						if (ADC_out1_mV<10) 
-						{lcd_i2c_w("00");
-						lcd_i2c_dec(ADC_out1_mV);
-						} 
-						if ((ADC_out1_mV<100)&(ADC_out1_mV>9))
-						{lcd_i2c_w("0");
-						lcd_i2c_dec(ADC_out1_mV);
-						} 
-						if	(ADC_out1_mV>99)	{lcd_i2c_dec(ADC_out1_mV);}
-						lcd_i2c_w("kV");
-						lcd_i2c_w("  ");
-						lcd_i2c_dec(ADC_out2_V);
-						lcd_i2c_w(",");
-		
-						if (ADC_out2_mV<10) 
-						{lcd_i2c_w("00");
-						lcd_i2c_dec(ADC_out2_mV);
-						} 
-						if ((ADC_out2_mV<100)&(ADC_out2_mV>9))
-						{lcd_i2c_w("0");
-						lcd_i2c_dec(ADC_out2_mV);
-						} 
-						if	(ADC_out2_mV>99)	lcd_i2c_dec(ADC_out2_mV);
-						//ADC1->CR2=0x0007;
-						lcd_i2c_w("A ");
-						ADC_out1=0;
-						ADC_out2=0;
-						//Averaging
-						for (int i=0;i<10;i++)			
-						{	
-			
-						ADC1->CR2=0x00000001;									//mesure start ADC1
-						ADC2->CR2=0x00000001;									//mesure start ADC2
-						_delay_ms(1);
-						ADC_out1= ADC_out1+ADC_mV(ADC1->DR);
-						ADC_out2= ADC_out2+ADC_mV(ADC2->DR);
-							}//end Averaging
-						}
+				if (ADC_out1_mV<10) 
+				{lcd_i2c_w("00");
+				lcd_i2c_dec(ADC_out1_mV);
+				} 
+				if ((ADC_out1_mV<100)&(ADC_out1_mV>9))
+				{lcd_i2c_w("0");
+				lcd_i2c_dec(ADC_out1_mV);
+				} 
+				if	(ADC_out1_mV>99)	{lcd_i2c_dec(ADC_out1_mV);}
+				lcd_i2c_w("kV");
+				lcd_i2c_w("  ");
+				lcd_i2c_dec(ADC_out2_V);
+				lcd_i2c_w(",");
+
+				if (ADC_out2_mV<10) 
+				{lcd_i2c_w("00");
+				lcd_i2c_dec(ADC_out2_mV);
+				} 
+				if ((ADC_out2_mV<100)&(ADC_out2_mV>9))
+				{lcd_i2c_w("0");
+				lcd_i2c_dec(ADC_out2_mV);
+				} 
+				if	(ADC_out2_mV>99)	lcd_i2c_dec(ADC_out2_mV);
+				//ADC1->CR2=0x0007;
+				lcd_i2c_w("A ");
+				ADC_out1=0;
+				ADC_out2=0;
+				//Averaging
+				for (int i=0;i<10;i++)			
+				{	
+	
+				ADC1->CR2=0x00000001;									//mesure start ADC1
+				ADC2->CR2=0x00000001;									//mesure start ADC2
+				_delay_ms(1);
+				ADC_out1= ADC_out1+ADC_mV(ADC1->DR);
+				ADC_out2= ADC_out2+ADC_mV(ADC2->DR);
+					}//end Averaging
+				}
  
             clear_RXBuffer();
         }
